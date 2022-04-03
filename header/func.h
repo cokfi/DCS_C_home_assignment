72278f92b84130acf98af41b197571261e2a0a82
Kfir Cohen <cokfi@post.bgu.ac.il>
2022-04-03 15:46:23 +0300

    header in right folder

---
+++ header/func.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef _func_H_
+#define _func_H_
+
+#define SIZE 6
+
+extern char** menu[];
+char** vehicles[];
+char** currency[];
+char** animals[];
+
+
+extern void show(char** strlst);
+
+
+#endif
