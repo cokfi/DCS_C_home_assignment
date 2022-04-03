3b4a54e8129dccfac66653102e3999291ab68847
Kfir Cohen <cokfi@post.bgu.ac.il>
2022-04-03 15:41:36 +0300

    add files

---
+++ DCS_C_home_assignment/func.h
@@ -0,0 +1,23 @@
+#ifndef _func_H_
+#define _func_H_
+
+#define SIZE 6
+
+extern char** menu[];
+char** vehicles[];
+char** currency[];
+char** animals[];
+
+
+extern void show(char** strlst);
+
+
+#endif
+
+
+
+
+
+
+
+
